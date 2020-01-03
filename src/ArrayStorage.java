c51158a5d51f042228f3888096a0a1a42ed3050f
flenn <deswier@bk.ru>
2020-01-03 18:09:26 +0700

    Edited functions in the class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,60 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-
+    private Resume[] storage = new Resume[10000];
+    private int size=0;
     void clear() {
+        for(int i=0; i<size; i++)
+        {
+            storage[i]=null;
+        }
+        size=0;
     }
 
     void save(Resume r) {
+        if(r.uuid!=null) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++)
+        {
+            if(storage[i].uuid.equals(uuid))
+            {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++)
+        {
+            if(storage[i].uuid.equals(uuid))
+            {
+                if(i!=size-1) {
+                    storage[i] = storage[size - 1];
+                    storage[size-1]=null;
+                }
+                else  storage[i]=null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] fullStorage = new Resume[size];
+        System.arraycopy(storage, 0, fullStorage, 0, size);
+        return fullStorage                                                                                                                  ;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
