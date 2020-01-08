ca10c616d4bf076a2483897a3e4b4503a958d7e9
flenn <deswier@bk.ru>
2020-01-08 19:11:33 +0700

    Return number of resume in function isExist

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,20 +12,18 @@ public class ArrayStorage {
         size = 0;
     }
 
-    private boolean isExist(String uuid) {
-        boolean isExist = false;
+    private int isExist(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                isExist = true;
-                break;
+                return i;
             }
         }
-        return isExist;
+        return -1; // THE RESUME IS NOT PRESENT
     }
 
     void save(Resume r) {
         if (r.uuid != null) {
-            if (!isExist(r.uuid)) {
+            if (isExist(r.uuid) == -1) {
                 storage[size] = r;
                 size++;
             } else System.out.println("\nResume is present");
@@ -42,22 +40,20 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                if (i != size - 1) {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                } else storage[i] = null;
-                size--;
-                break;
-            }
+        int exist = isExist(uuid);
+        if (exist != -1) {
+            if (exist != size - 1) {
+                storage[exist] = storage[size - 1];
+                storage[size - 1] = null;
+            } else storage[exist] = null;
+            size--;
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-        Resume[] getAll() {
+    Resume[] getAll() {
         Resume[] resumes = new Resume[size];
         System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
