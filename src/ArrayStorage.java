bb10a5c5b902ab1e5d1849aaa259353e12327f82
flenn <deswier@bk.ru>
2020-01-08 18:47:47 +0700

    Change parameter name in the function checkPresent

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,10 +12,10 @@ public class ArrayStorage {
         size = 0;
     }
 
-    private boolean checkPresent(Resume r) {
+    private boolean checkPresent(String uuid) {
         boolean isExist = false;
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(r.uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 isExist = true;
                 break;
             }
@@ -23,10 +23,9 @@ public class ArrayStorage {
         return isExist;
     }
 
-
     void save(Resume r) {
         if (r.uuid != null) {
-            if (!checkPresent(r)) {
+            if (!checkPresent(r.uuid)) {
                 storage[size] = r;
                 size++;
             } else System.out.println("\nResume is present");
@@ -58,7 +57,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+        Resume[] getAll() {
         Resume[] resumes = new Resume[size];
         System.arraycopy(storage, 0, resumes, 0, size);
         return resumes;
--- src/MainArray.java
+++ src/MainArray.java
@@ -13,7 +13,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit ): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -56,7 +56,7 @@ public class MainArray {
         }
     }
 
-    static void printAll() {
+    private static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
