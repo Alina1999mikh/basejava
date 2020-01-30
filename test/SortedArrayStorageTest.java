a1b0b7f2829cd6283f1aec1eabc7d4a7efc5db0a
flenn <deswier@bk.ru>
2020-01-30 22:44:30 +0700

    No change

---
+++ test/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+import com.urise.webapp.storage.SortedArrayStorage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
