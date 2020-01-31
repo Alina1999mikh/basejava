8f42e91953ed2133fc9c82e0aba13f7358a9e376
flenn <deswier@bk.ru>
2020-01-31 16:07:13 +0700

    Move the test folder

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,9 +1,9 @@
+package com.urise.webapp.storage;
+
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.AbstractArrayStorage;
-import com.urise.webapp.storage.Storage;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -76,7 +76,6 @@ abstract public class AbstractArrayStorageTest {
         }
         storage.save(new Resume());
     }
-
 
     @Test(expected = NotExistStorageException.class)
     public void delete() {
